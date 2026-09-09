3a6d8ee53665599fa1515929e0bc1538fe1d9b67
accesswarrior <okoezegodswill099@gmail.com>
2026-09-09 09:50:27 +0100

    Update firebaseConfig.js

--- js/core/firebaseConfig.js
+++ js/core/firebaseConfig.js
@@ -1,19 +1,14 @@
 // js/core/firebaseConfig.js
-
-// Firebase configuration (compat SDK)
 const firebaseConfig = {
   apiKey: "AIzaSyBwJzpnmSV845YrDDZYOVntL6sfFAVGaag",
   authDomain: "access-warrior-1d789.firebaseapp.com",
   projectId: "access-warrior-1d789",
   storageBucket: "access-warrior-1d789.firebasestorage.app",
   messagingSenderId: "875315539922",
-  appId: "1:875315539922:web:df434dfd4316c0a457620b",
-  measurementId: "G-N9JLL90K1E"
+  appId: "1:875315539922:web:df434dfd4316c0a457620b"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Get references to Firebase services
 const auth = firebase.auth();
 const db = firebase.firestore();
